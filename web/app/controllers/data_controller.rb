5386437d41262e57c5521b1aca9e9480c19fd27e
Andrea Epifani <andrea@andreaepifani.com>
2015-07-04 04:20:22 +1000

    More datasets added; basic API endpoints set up.

---
+++ web/app/controllers/data_controller.rb
@@ -0,0 +1,82 @@
+class DataController < ActionController::Base
+  # Prevent CSRF attacks by raising an exception.
+  # For APIs, you may want to use :null_session instead.
+  protect_from_forgery with: :exception
+
+  def locations
+
+  	suburb = params[:suburb]
+  	location = params[:status]
+  	category = params[:category]
+  	rating = params[:rating]
+
+  	file = File.read(Rails.root.join('public', 'locations_accessibility.json'))
+  	data = JSON.parse(file)
+
+  	return_data = []
+
+  	if suburb and !category
+	  	data.each do |child|
+	  		if child['suburb'].downcase.include? suburb.downcase
+	    		return_data.push(child)
+	    	end
+		end
+	end
+
+	if category and !suburb
+	  	data.each do |child|
+	  		if child['category'].downcase.include? category.downcase
+	    		return_data.push(child)
+	    	end
+		end
+	end
+
+  	render :json => return_data
+
+  end
+
+  def qr
+
+  	station = params[:station]
+
+  	file = File.read(Rails.root.join('public', 'qr_accessibility.json'))
+  	data = JSON.parse(file)
+
+  	return_data = []
+
+  	if station
+	  	data.each do |child|
+	  		if child['Station'].downcase.eql? station.downcase
+	    		return_data.push(child)
+	    	end
+		end
+	end
+
+  	render :json => return_data
+
+  end
+
+  def areas
+
+  	suburb = params[:suburb]
+
+  	file = File.read(Rails.root.join('public', 'suburbs_accessibility.json'))
+  	data = JSON.parse(file)
+
+  	return_data = []
+
+  	if suburb
+	  	data.each do |child|
+	  		if child['Suburb_Name'].downcase.eql? suburb.downcase
+	    		return_data.push(child)
+	    	end
+		end
+	end
+
+  	render :json => return_data
+
+  end
+
+
+
+end
--- web/config/routes.rb
+++ web/config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
+  get 'data/locations' => 'data#locations'
+  get 'data/qr' => 'data#qr'
+  get 'data/areas' => 'data#areas'
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
