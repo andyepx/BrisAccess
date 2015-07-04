03597610bc0fcaaf6b6680f54fb65d2885447b68
Andrea Betarani Vincita <abvincita@gmail.com>
2015-07-04 14:30:14 +1000

    Get journey planner to work (return url).

--- app/app/src/main/java/com/whatamelon/brisaccess/MainActivity.java
+++ app/app/src/main/java/com/whatamelon/brisaccess/MainActivity.java
@@ -1,13 +1,33 @@
 package com.whatamelon.brisaccess;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.net.Uri;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.widget.SimpleCursorAdapter;
+import android.provider.SyncStateContract.Columns;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.format.DateFormat;
 import android.util.Log;
 import android.location.Location;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.FilterQueryProvider;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.view.View.OnClickListener;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
@@ -21,10 +41,22 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity implements
         GoogleApiClient.ConnectionCallbacks,
         GoogleApiClient.OnConnectionFailedListener,
-        LocationListener
+        LocationListener,
+        JSONRequest.NetworkListener,
+        OnClickListener,
+        OnItemClickListener
 {
     final String TAG = "BrisAccess";
     private static final LatLng DEFAULT_LOCATION = new LatLng(-27.498037,153.017823);
@@ -36,6 +68,26 @@ public class MainActivity extends AppCompatActivity implements
     private SupportMapFragment mapFrag;
     private GoogleMap mMap;
     private Toolbar toolbar;
+    private AutoCompleteTextView destInput;
+
+    private JSONRequest request;
+    private HashMap<String, String> locationsToID = new HashMap<>();
+    private List<String> requestParameters = new ArrayList<>();
+
+    // Date/Time Settings
+    private static Button dateButton;
+    private static Button timeButton;
+    static String date;
+    static String time;
+    static String dateDisplay;
+    final static Calendar c = Calendar.getInstance();
+    static int year;
+    static int month;
+    static int day;
+    static int hour;
+    static int minute;
+    static Calendar currentDate = Calendar.getInstance();
+    static Calendar selectedDate = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -53,6 +105,44 @@ public class MainActivity extends AppCompatActivity implements
                 .build();
 
         mapInit();
+
+        SimpleCursorAdapter cursorAdapter = getSimpleCursorAdapter();
+        destInput = (AutoCompleteTextView) findViewById(R.id.dest_input);
+        destInput.setAdapter(cursorAdapter);
+        destInput.setThreshold(2);
+
+        year = c.get(Calendar.YEAR);
+        month = c.get(Calendar.MONTH);
+        day = c.get(Calendar.DAY_OF_MONTH);
+        hour = c.get(Calendar.HOUR_OF_DAY);
+        minute = c.get(Calendar.MINUTE);
+
+        currentDate.clear();
+        currentDate.set(year, month, day);
+
+        selectedDate.clear();
+        selectedDate.set(year, month, day);
+
+        date = (month+1) + "/" + day + "/" + year;
+        dateDisplay = day + " / " + (month+1) + " / " + year;
+
+        String temp;
+        if(minute < 10)
+            temp = "0" + minute;
+        else
+            temp = "" + minute;
+
+        time = hour + ":" + temp;
+
+        dateButton = (Button) findViewById(R.id.dateSpinner);
+        timeButton = (Button) findViewById(R.id.timeSpinner);
+        dateButton.setText(dateDisplay);
+        timeButton.setText(time);
+        dateButton.setOnClickListener(this);
+        timeButton.setOnClickListener(this);
+
+        Button goButton = (Button) findViewById(R.id.go_button);
+        goButton.setOnClickListener(this);
     }
 
     public void mapInit() {
@@ -123,5 +213,189 @@ public class MainActivity extends AppCompatActivity implements
         userMarker.setPosition(userLatLng);
 
         LocationServices.FusedLocationApi.removeLocationUpdates(googleApiClient, this);
+    }
+
+    private SimpleCursorAdapter getSimpleCursorAdapter()
+    {
+        String[] from = { "locations" };
+        int[] to = { android.R.id.text1 };
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this, android.R.layout.simple_spinner_dropdown_item, null, from, to, 0);
+        adapter.setStringConversionColumn(1);
+
+        FilterQueryProvider provider = new FilterQueryProvider()
+        {
+            @Override
+            public Cursor runQuery(CharSequence constraint)
+            {
+                if (constraint == null || constraint.toString().equals("Current location"))
+                    return null;
+
+                String[] columnLocations = { Columns._ID, "locations" };
+                MatrixCursor locationMatrix = new MatrixCursor(columnLocations);
+                try
+                {
+                    String url = "http://abvincita.com/transmob/resolve.php?input="
+                            + Uri.encode(constraint.toString())
+                            + "&maxResults=" + Uri.encode("5");
+
+                    request = new JSONRequest();
+                    request.setListener(MainActivity.this);
+                    request.execute(url);
+
+                    String result = request.get();
+
+                    Object obj = JSONValue.parse(result);
+                    JSONArray array = (JSONArray)((JSONObject)obj).get("Suggestions");
+
+                    for(int i = 0; i < array.size(); i++)
+                    {
+                        JSONObject location = (JSONObject) array.get(i);
+                        String desc = (String) location.get("Description");
+                        String id = (String) location.get("Id");
+
+                        String[] splitted = desc.split(" \\(");
+                        if (splitted.length > 1)
+                            desc = splitted[0];
+
+                        locationsToID.put(desc, id);
+                        locationMatrix.newRow().add(i).add(desc);
+                    }
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+                return locationMatrix;
+            }
+        };
+
+        adapter.setFilterQueryProvider(provider);
+        return adapter;
+    }
+
+    @Override
+    public void onClick(View v)
+    {
+        DialogFragment newFragment;
+
+        switch (v.getId())
+        {
+            case R.id.dateSpinner:
+                newFragment = new DatePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "datePicker");
+                break;
+
+            case R.id.timeSpinner:
+                newFragment = new TimePickerFragment();
+                newFragment.show(getSupportFragmentManager(), "timePicker");
+                break;
+
+            case R.id.go_button:
+
+                requestParameters.add(0, "GP:" + userLatLng.latitude + "," + userLatLng.longitude);
+
+                String destination = destInput.getText().toString();
+                String destinationID = locationsToID.get(destination);
+
+                if(destinationID == null)
+                {
+                    Toast.makeText(this,
+                            "Please enter a valid destination.", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                requestParameters.add(1, destinationID);
+                requestParameters.add(date + " " + time);
+
+                requestPlan(requestParameters.get(0),
+                            requestParameters.get(1),
+                            requestParameters.get(2));
+
+                break;
+        }
+    }
+
+    private void requestPlan(String fromId, String destId, String date)
+    {
+        //showProgressBar(true);
+
+		/* Call our php code on web zone */
+        String urlString = "http://abvincita.com/transmob/journeyplan.php?fromLocId="
+                + Uri.encode(fromId) + "&destLocId=" + Uri.encode(destId) + "&date=" + Uri.encode(date)
+                + "&leaveOption=" + Uri.encode("0")
+                + "&vehicleTypes=" + Uri.encode("0")
+                + "&maxWalkDistance=" + Uri.encode("1000");
+        Log.d("JourneyMap request: ", urlString);
+        request = new JSONRequest();
+        request.setListener(this);
+        request.execute(urlString);
+
+        try {
+            String result = request.get();
+            Toast.makeText(this, result, Toast.LENGTH_LONG).show();
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id)
+    {
+        //Just dismissing keyboard
+        InputMethodManager imm = (InputMethodManager) getSystemService(this.INPUT_METHOD_SERVICE);
+        if(imm.isAcceptingText())
+            imm.hideSoftInputFromWindow(getCurrentFocus().getWindowToken(), 0);
+    }
+
+    @Override
+    public void networkRequestCompleted(String result)
+    {
+        Log.d("NETWORK REQ COMPLETED", result);
+    }
+
+    public static class DatePickerFragment extends DialogFragment implements
+            DatePickerDialog.OnDateSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState)
+        {
+            return new DatePickerDialog(getActivity(), this, year, month, day);
+        }
+
+        public void onDateSet(DatePicker view, int year, int month, int day)
+        {
+            selectedDate.clear();
+            selectedDate.set(year, month, day);
+
+            // Do something with the date chosen by the user
+            date = (month+1) + "/" + day + "/" + year;
+            dateDisplay = day + " / " + (month+1) + " / " + year;
+            dateButton.setText(dateDisplay);
+        }
+    }
+
+    public static class TimePickerFragment extends DialogFragment implements
+            TimePickerDialog.OnTimeSetListener {
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState)
+        {
+            return new TimePickerDialog(getActivity(), this, hour, minute,
+                    DateFormat.is24HourFormat(getActivity()));
+        }
+
+        public void onTimeSet(TimePicker view, int hourOfDay, int minute)
+        {
+            String temp;
+            if(minute < 10)
+                temp = "0" + minute;
+            else
+                temp = "" + minute;
+
+            time = hourOfDay + ":" + temp;
+            timeButton.setText(time);
+        }
     }
 }
